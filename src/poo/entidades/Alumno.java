c7fa62f29949579892c6248ef69fcef78fe91886
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-06-02 21:08:18 -0600

    feat: crear entidad Alumno - Jose Angel Mate Carias

--- src/poo/entidades/Alumno.java
+++ src/poo/entidades/Alumno.java
@@ -1,4 +1,51 @@
 package poo.entidades;
 
 public class Alumno {
+
+    private int codigo;
+    private String nombre;
+    private String carnet;
+
+    public Alumno() {
+
+    }
+
+    public Alumno(int codigo, String nombre, String carnet) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.carnet = carnet;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "codigo=" + codigo +
+                ", nombre='" + nombre + '\'' +
+                ", carnet='" + carnet + '\'' +
+                '}';
+    }
 }
