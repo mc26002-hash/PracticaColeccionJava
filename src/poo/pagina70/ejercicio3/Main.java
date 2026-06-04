6fc5ec7f528b03ec2728f50433d6be595c063821
Oscar Ramirez <barleyunmain@gmail.com>
2026-06-04 16:34:24 -0600

    Se realizo el ejercicio 3 Realizado por : Oscar Rmirez

--- src/poo/pagina70/ejercicio3/Main.java
+++ src/poo/pagina70/ejercicio3/Main.java
@@ -1,4 +1,25 @@
 package poo.pagina70.ejercicio3;
 
+import poo.entidades.Alumno;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList<Alumno> lista = new LinkedList<>();
+
+        // Insertar alumnos
+        lista.add(new Alumno(1, "Oscar Ramirez", "RM25001"));
+        lista.add(new Alumno(2, "Maria Lopez", "RM25002"));
+        lista.add(new Alumno(3, "Carlos Perez", "RM25003"));
+
+        // Mostrar alumnos
+        Iterator<Alumno> it = lista.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
 }
