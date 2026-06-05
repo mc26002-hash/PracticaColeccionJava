22a7ffa31cf5adc12ebcaedcadbecbd1707a68c2
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-06-04 20:29:02 -0600

    Creacion de la Entidad Libro - Jose Angel Mate Carias

--- src/poo/entidades/Libro.java
+++ src/poo/entidades/Libro.java
@@ -1,4 +1,63 @@
 package poo.entidades;
 
 public class Libro {
+
+    private int id;
+    private String titulo;
+    private String edicion;
+    private String autor;
+
+    public Libro() {
+
+    }
+
+    public Libro(int id, String titulo, String edicion, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.edicion = edicion;
+        this.autor = autor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(String edicion) {
+        this.edicion = edicion;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", edicion='" + edicion + '\'' +
+                ", autor='" + autor + '\'' +
+                '}';
+    }
 }
