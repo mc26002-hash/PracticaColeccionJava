7ef837b4bdb2cdd121b1f73dbe81b26ad5af32fb
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-06-04 20:36:59 -0600

    Finalizacion del ejercicio 1 - Jose Angel Mate Carias
    primer ejercicio de la pagina 75

--- src/poo/pagina75/ejercicio1/Main.java
+++ src/poo/pagina75/ejercicio1/Main.java
@@ -1,4 +1,34 @@
 package poo.pagina75.ejercicio1;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        TreeSet<Double> numeros = new TreeSet<>();
+
+        String respuesta;
+
+        do {
+            System.out.print("Ingrese un número: ");
+            double numero = scanner.nextDouble();
+
+            numeros.add(numero);
+
+            System.out.print("¿Desea ingresar otro número? (S/N): ");
+            respuesta = scanner.next();
+
+        } while (respuesta.equalsIgnoreCase("S"));
+
+        System.out.println("\nNúmeros ordenados de menor a mayor:");
+
+        for (Double numero : numeros) {
+            System.out.println(numero);
+        }
+
+        scanner.close();
+    }
 }
