5073d1eb8ea7cc1f6494efe87622f806b9c82092
abigail23181 <abigailrodrigxz@gmail.com>
2026-06-05 22:01:43 -0600

    Ana realizo Ejemplo2ArrayList

--- src/poo/ejemplos_list/ejemplo2arraylist/Main.java
+++ src/poo/ejemplos_list/ejemplo2arraylist/Main.java
@@ -1,4 +1,38 @@
 package poo.ejemplos_list.ejemplo2arraylist;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
+    public static void main(String[] args) {
+        //crear una colección de un solo tipo
+        ArrayList<String> lista = new ArrayList<String>();
+        int i = 1;
+
+        //asignamos valores a la colección
+        while(i <= 50)
+        {
+            lista.add("Cadena número " + i);
+            i++;
+        }
+
+        lista.add(25, "3.1416"); //agregamos elemento en una posición específica
+
+        //recorremos la colección para mostrar los elementos
+        for(String item:lista)
+        {
+            System.out.println(item);
+        }
+
+        //aplicación del método remove()
+        lista.remove("Cadena número 25");
+        lista.remove(40);
+
+        System.out.println("\n");
+        Iterator<String> it = lista.iterator();
+        while(it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+    }
 }
