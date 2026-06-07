9a2f410db85d17888b043a5c603b9506de76ce11
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-06-06 19:18:18 -0600

    Ejemplos de arraylistvslinkedlist Completedos - Jose Angel Mate Carias

--- src/poo/ejemplos_list/arraylistvslinkedlist/Main.java
+++ src/poo/ejemplos_list/arraylistvslinkedlist/Main.java
@@ -1,4 +1,51 @@
 package poo.ejemplos_list.arraylistvslinkedlist;
 
+import poo.entidades.Persona;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Declarar dos colecciones
+        List<Persona> listaArray = new ArrayList<>();
+        List<Persona> listaLinked = new LinkedList<>();
+
+        long antes;
+
+        // Llenar las listas con elementos
+        for (int i = 0; i <= 10000; i++) {
+            listaArray.add(new Persona(i, "Persona " + i, "Tel. " + i));
+            listaLinked.add(new Persona(i, "Persona " + i, "Tel. " + i));
+        }
+
+        System.out.println("Tiempo en insertar una Persona en ArrayList");
+        antes = System.nanoTime();
+        listaArray.add(0, new Persona(10001, "Prueba inicio", "000000"));
+        System.out.println(System.nanoTime() - antes + " ns");
+
+        System.out.println();
+
+        System.out.println("Tiempo en insertar una Persona en LinkedList");
+        antes = System.nanoTime();
+        listaLinked.add(0, new Persona(10001, "Prueba inicio", "000000"));
+        System.out.println(System.nanoTime() - antes + " ns");
+
+        System.out.println();
+
+        System.out.println("Tiempo en insertar en ArrayList al centro");
+        antes = System.nanoTime();
+        listaArray.add(5000, new Persona(10002, "Prueba centro", "000000"));
+        System.out.println(System.nanoTime() - antes + " ns");
+
+        System.out.println();
+
+        System.out.println("Tiempo en insertar en LinkedList al centro");
+        antes = System.nanoTime();
+        listaLinked.add(5000, new Persona(10002, "Prueba centro", "000000"));
+        System.out.println(System.nanoTime() - antes + " ns");
+    }
 }
--- src/poo/ejemplos_set/colecciontreeset/Main.java
+++ src/poo/ejemplos_set/colecciontreeset/Main.java
@@ -1,4 +1,48 @@
 package poo.ejemplos_set.colecciontreeset;
 
+import poo.entidades.Equipo;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Crear la colección TreeSet
+        Set<String> lista = new TreeSet<>();
+
+        lista.add("hola");
+        lista.add("que tal?");
+        lista.add("ya casi es navidad");
+        lista.add("programadores");
+        lista.add("hola");
+        lista.add("el sabado hay clases");
+
+        // Recorrer la colección
+        Iterator<String> it = lista.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println();
+        System.out.println("****************** LISTA 2 ******************");
+
+        // Declaración de colección de tipo Equipo
+        TreeSet<Equipo> equipos = new TreeSet<>();
+
+        equipos.add(new Equipo(4, "Once Lobos FC", "El Salvador"));
+        equipos.add(new Equipo(1, "Sonsonate FC", "El Salvador"));
+        equipos.add(new Equipo(3, "CD FAS", "El Salvador"));
+        equipos.add(new Equipo(5, "Municipal Limeño", "El Salvador"));
+        equipos.add(new Equipo(2, "Aguila FC", "El Salvador"));
+        equipos.add(new Equipo(1, "Chalatenango FC", "El Salvador"));
+
+        // Mostrar equipos
+        for (Equipo eq : equipos) {
+            System.out.println(eq.getId() + " " + eq.getNombre());
+        }
+    }
 }
